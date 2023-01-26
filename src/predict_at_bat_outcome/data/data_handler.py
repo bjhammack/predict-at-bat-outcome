46f7cb746d1f8e97090371a79c568a6c6c2ab56e
Benjamin Hammack <bjhammack@protonmail.com>
2023-01-25 23:11:57 -0600

    Cleaned up data_handler.py and test_data.py.
    - flake8'd and mypy'd data_handler.py and test_data.py
    - Added comments to each test function in test_data.py
    - Added some typing to test_data.py

--- src/predict_at_bat_outcome/data/data_handler.py
+++ src/predict_at_bat_outcome/data/data_handler.py
@@ -2,6 +2,7 @@ from glob import glob
 import pandas as pd
 from typing import Optional
 
+
 class Data:
     '''
     Class that will contain the data for modeling and manages all functions in
@@ -16,9 +17,10 @@ class Data:
     test split sizes.
     source -- string; path to the original source of the data.
     '''
-    def __init__(self,
-            source: Optional[str]=None,
-            data: Optional[pd.DataFrame]=None
+    def __init__(
+            self,
+            source: Optional[str] = None,
+            data: Optional[pd.DataFrame] = None
             ):
         '''
         Initialize the Data class by reading in the data.
@@ -27,7 +29,7 @@ class Data:
         source -- string; directory or CSV where raw data is stored.
         data -- pandas DataFrame; pre-collected data.
         '''
-        self.data = self.train = self.dev = self.test = self.split = self.source = None
+        self.data = self.train = self.dev = self.test = self.split = self.source = None  # noqa: E501
 
         if data:
             self.data = data
@@ -37,7 +39,8 @@ class Data:
 
     def collect_data(self, source: str) -> pd.DataFrame:
         '''
-        Returns collected data from given source directory and all sub-directories.
+        Returns collected data from given source directory and all
+        sub-directories.
 
         Args:
         source -- string; directory or CSV where data is stored.
@@ -51,9 +54,10 @@ class Data:
             csv_files = glob(f'{source}/**/*.csv', recursive=True)
 
         if not csv_files:
-            raise ValueError(f'No CSV files found at "{source}". '
-                              'Check that your path is correct.')
-        
+            raise ValueError(
+                f'No CSV files found at "{source}". '
+                'Check that your path is correct.')
+
         dfs = []
         for csv in csv_files:
             dfs.append(pd.read_csv(csv))
@@ -67,7 +71,7 @@ class Data:
                 f'CSV 1 and CSV {i+1}\'s columns do not match. All columns '
                 f'must match to be properly concatenated.\nCSV 1: {df_cols}'
                 f'\nCSV {i+1}: {df_comp_cols}')
-        
+
         data = pd.concat(dfs, ignore_index=True)
 
         return data
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,43 +1,71 @@
-from src.predict_at_bat_outcome.data.data_handler import Data
+from src.predict_at_bat_outcome.data import data_handler
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import pytest
+from typing import Dict
+
+
+@pytest.fixture(scope='module')
+def data_class() -> data_handler.Data:
+    '''
+    Returns Data class from data_handler to be used during a module's testing.
+    '''
+    return data_handler.Data()
 
 
 @pytest.fixture(scope='class')
-def read_data():
-    data_class = Data()
+def read_data() -> Dict[str, pd.DataFrame]:
+    '''
+    Returns dict of two pd.DataFrames to validate the TestReadingData tests.
+    '''
     test_data1 = pd.read_csv('tests/data/validation_data/full_dummy1.csv')
     test_data2 = pd.read_csv('tests/data/validation_data/full_dummy2.csv')
     
-    return {
-        'Data': data_class,
-        'test_data1': test_data1,
-        'test_data2': test_data2
-        }
+    return {'test_data1': test_data1, 'test_data2': test_data2}
 
 
 class TestReadingData:
-    def test_good_path_single_csv(self, read_data):
-        df = read_data['Data'].collect_data('tests/data/dummy_data1/test_data1.csv')
+    '''
+    Suite of tests to test the reading in of data by data_handler.py.
+    '''
+    def test_good_path_single_csv(self, data_class, read_data):
+        '''
+        Tests if collect_data() can gather data from single DF.
+        '''
+        df = data_class.collect_data('tests/data/dummy_data1/test_data1.csv')
         assert_frame_equal(df, read_data['test_data1'])
 
-    def test_good_path_directory(self, read_data):
-        df = read_data['Data'].collect_data('tests/data/dummy_data1')
+    def test_good_path_directory(self, data_class, read_data):
+        '''
+        Tests if collect_data() can gather data from a directory.
+        '''
+        df = data_class.collect_data('tests/data/dummy_data1')
         assert_frame_equal(df, read_data['test_data1'])
 
-    def test_bad_path(self, read_data):
+    def test_bad_path(self, data_class):
+        '''
+        Tests if collect_data() raises the correct error w/ no CSV files.
+        '''
         with pytest.raises(ValueError, match='No CSV files'):
-            read_data['Data'].collect_data('tests/data/doesnt_exist')
+            data_class.collect_data('tests/data/doesnt_exist')
 
-    def test_empty_path(self, read_data):
+    def test_empty_path(self, data_class):
+        '''
+        Tests if collect_data() raises the correct error for an empty dir.
+        '''
         with pytest.raises(ValueError, match='No CSV files'):
-            read_data['Data'].collect_data('tests/data/dummy_data4')
+            data_class.collect_data('tests/data/dummy_data4')
 
-    def test_sub_directories(self, read_data):
-        df = read_data['Data'].collect_data('tests/data/dummy_data2/')
+    def test_sub_directories(self, data_class, read_data):
+        '''
+        Tests if collect_data() can gather data from multiple sub-directories.
+        '''
+        df = data_class.collect_data('tests/data/dummy_data2/')
         assert_frame_equal(df, read_data['test_data2'])
 
-    def test_bad_concat(self, read_data):
-        with pytest.raises(pd.errors.DataError, match='All columns must match'):
-            read_data['Data'].collect_data('tests/data/dummy_data3')
+    def test_bad_concat(self, data_class):
+        '''
+        Tests if collect_data() raises the correct error for mismatching DFs.
+        '''
+        with pytest.raises(pd.errors.DataError, match='All columns must'):
+            data_class.collect_data('tests/data/dummy_data3')
